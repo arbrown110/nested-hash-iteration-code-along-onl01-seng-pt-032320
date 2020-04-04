dfb24881c2a00b648d7f672ebd5d33e4b6939cc9
chic-caller-7520 <arbrown110@gmail.com>
2020-04-04 03:17:15 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_detail_hash|
     if person == "freddy Mercury" 
       contact_detail_hash.each do |attribute, data|
-    if     
+       if attribute == :favorite_ice_cream_flavors 
+         data.delete_if {|ice_cream|  ice_cream == "strawberry"} 
         
         end
       end
